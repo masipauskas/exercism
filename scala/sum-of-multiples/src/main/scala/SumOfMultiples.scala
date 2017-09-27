fd5c24c2b63cf48c36100ae52f72b59cd4ba6c83
Martynas Asipauskas <me@martynas.co.uk>
2017-09-27 01:52:17 +0100

    Implemented second code kata for Scala and Typescript

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,4 +1,8 @@
 object SumOfMultiples {
-  def sum(factors: Set[Int], limit: Int): Int = ???
+  def sum(factors: Set[Int], limit: Int): Int = {
+    def isMultiple(number: Int) = factors.exists(factor => number % factor == 0)
+
+    (1 until limit).foldLeft(0)((acc, num) => if (isMultiple(num)) acc + num else acc)
+  }
 }
 
